516b2d832339a3db1e728f96cd30bee13b3545dc
jongmin-chung <chungjm0711@gmail.com>
2025-05-27 01:56:13 +0900

    feat: openabcd-build-conventions

---
+++ openabcd-build-conventions/src/main/kotlin/io.github.openabcd.java-common-conventions.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("io.github.openabcd.common-conventions")
+
+    id("io.github.openabcd.maven-repository-conventions")
+
+    java
+
+    id("io.github.openabcd.java-formatter-conventions")
+}
